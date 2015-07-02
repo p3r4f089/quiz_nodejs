90bce5d013ea34349af8e4c836ec81ff3b5195e4
perafo89 <perafo89@gmail.com>
2015-07-02 18:15:14 -0500

    borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -69,3 +69,35 @@ exports.create = function(req, res){
     }
   ).catch(function(error){next(error)});
 };
+
+exports.edit = function(req, res){
+    var quiz = req.quiz;
+    res.render('quizes/edit', {quiz:quiz, errors: []});
+};
+
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if(err){
+        res.render('quizes/edit', {quiz: req.quiz , errors: err.errors });
+      }else{
+        req.quiz
+        .save({fields:["pregunta", "respuesta"]})
+        .then(function(){
+          res.redirect('/quizes');
+        })
+      }
+    }
+  ).catch(function(error){next(error)});
+};
+
+exports.destroy = function(req, res){
+  req.quiz.destroy().then(function(){
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
